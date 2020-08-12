86d60b5378cc9037bc73ea2031e86ce05634c646
JoshuaHolloway <jhollow6@asu.edu>
2020-08-12 10:34:08 -0500

    removeLastFlavor()

--- index.js
+++ index.js
@@ -59,11 +59,11 @@ Your function should add the flavor to the front of the array and console.log th
 
 For example addFlavor("Rainbow Sherbert", originalFlavors) should return ["Rainbow Sherbert", "Banana Nut Fudge",..."Vanilla Burnt Almond"] */ 
 
-function addFlavor(/*code here*/){
-
-    /*code here*/
-
-}
+const addFlavor = ((new_flavor, arr) =>{
+    arr.unshift(new_flavor);
+    console.log(arr);
+    console.log(`new array length: ${arr.length}`);
+})("Rainbow Sherbert", originalFlavors);
 
 
 /* Task 3: Houston, we have a problem! There are now 32 flavors in the array! Your task is to remove an item from the end of the array. 
@@ -76,11 +76,15 @@ Your function should remove a flavor from the end of the array and console.log t
 
 For example removeLastFlavor(originalFlavors) would return ["Rainbow Sherbert", "Banana Nut Fudge",..."Vanilla"]*/ 
 
-function removeLastFlavor(/*code here*/){
-
-    /*code here*/
-
-}
+const removeLastFlavor = ((arr) => {
+    console.log(`Inside removedLastFlavor function:\nThe input array has length: ${arr.length}`);
+    const popped = arr.pop();
+    console.log('The array after popping off the last element is:');
+    console.log(arr);
+    console.log(`The popped off element is: ${popped}`);
+    console.log(`The lenghth of the output array is ${arr.length}.\nNow leaving removeLastFlavor()`);
+    return popped;
+})(originalFlavors);
 
 /* Task 4: Write a function that returns a flavor at a given index in the array.
 
